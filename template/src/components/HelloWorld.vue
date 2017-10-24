a3ccba471589b0415818de5ca6953bff3e5478ef
Benjamin Vevang <benvev95@gmail.com>
2017-10-24 13:19:40 +0200

    Added hello world component

---
+++ template/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="container">
+  <h1>{{ msg }}</h1>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello, World!'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  h1 {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 5em;
+  }
+}
+</style>
+
