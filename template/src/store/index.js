f13d8e8b264d970cbe4070bf9cd80da74b8331d0
Benjamin Vevang <benvev95@gmail.com>
2017-10-24 16:09:37 +0200

    Setup Vuex generation and added back hello world from original template for general info, removed more airbnb ifs

---
+++ template/src/store/index.js
@@ -0,0 +1,32 @@
+const state = {
+  helloWorld: 'Hello, World!'
+}
+
+const getters = {
+  helloWorld: state.helloWorld
+}
+
+const actions = {
+  setHelloMessage ({ commit }, message) {
+    commit('setHelloMessage', message)
+  }
+}
+
+const mutations = {
+  setHelloMessage (state, payload) {
+    state.helloWorld = payload
+  }
+}
+
+export default {
+  modules: {
+    // Add modules here
+  },
+  state,
+  getters,
+  actions,
+  mutations,
+  // Use strict mode in development to show errors when
+  // state getters have been manipulated from outside the mutations
+  strict: process.env.NODE_ENV === 'development'
+}
