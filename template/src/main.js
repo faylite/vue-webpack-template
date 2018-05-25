6a6a11a4e01e16f01575111c1384927a899bb688
Benjamin Vevang <benvev95@gmail.com>
2018-05-25 11:10:35 +0200

    Removed choice between runtime-only and standalone Vue

--- template/src/main.js
+++ template/src/main.js
@@ -1,7 +1,5 @@
-{{#if_eq build "standalone"}}
 // The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-{{/if_eq}}
+// has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 {{#axios}}
 import Axios from 'axios'
@@ -62,11 +60,6 @@ new Vue({
   {{#store}}
   store: Store,
   {{/store}}
-  {{#if_eq build "runtime"}}
-  render: h => h(App)
-  {{/if_eq}}
-  {{#if_eq build "standalone"}}
   template: '<App/>',
   components: { App }
-  {{/if_eq}}
 })
